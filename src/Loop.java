a9f46572221611a9cb0c8e985e764bc2e4643668
unknown <eng.yanahid@gmai.com>
2023-02-05 00:19:33 +0600

    first commit

---
+++ src/Loop.java
@@ -0,0 +1,8 @@
+public class Loop {
+    public static void main(String[] args) {
+        int number = 10;
+        for (int i=0; i<number; i++) {
+            System.out.print(i);
+        }
+    }
+}
