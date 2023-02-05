9b64e8711f6ec47d42a7c69a3048fc72b42d124a
nahid <eng.yanahid@gmail.com>
2023-02-05 22:47:13 +0600

    ArithmaticDemo

---
+++ src/Operator.java
@@ -0,0 +1,25 @@
+public class Operator {
+
+    public static void main(String[] args) {
+
+        int num1, num2, result;
+
+        num1 = 20;
+        num2 = 10;
+
+        result = num1 + num2;
+        System.out.println("Sum = :"+result);
+
+        result = num1 - num2;
+        System.out.println("Sub = :"+result);
+
+        result = num1 * num2;
+        System.out.println("Multiplicatio = :"+result);
+
+        result = num1 / num2;
+        System.out.println("Div = :"+result);
+
+        result = num1 % num2;
+        System.out.println("Remainder = :"+result);
+    }
+}
