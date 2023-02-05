74baadd95ad0c95edab2c8a6f6c8004c69b9dd6e
nahid <eng.yanahid@gmail.com>
2023-02-05 22:27:36 +0600

    input demo 2

---
+++ src/Intup.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Intup {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+
+        System.out.print("Enter any number :");
+        number = input.nextInt();
+        System.out.println("number = "+number);
+
+
+    }
+}
