db6276ad1a400b1241a36f8a96a749ec0d219059
nahid <eng.yanahid@gmail.com>
2023-02-05 22:59:12 +0600

    ArithmaticInut

---
+++ src/ArithmaticInput.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ArithmaticInput {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int num1, num2, result;
+
+        System.out.print("Enter first number :");
+        num1 = input.nextInt();
+
+        System.out.print("Enter seocond number :");
+        num2 = input.nextInt();
+
+        result = num1 + num2;
+        System.out.println("Sum = :"+result);
+
+        result = num1 - num2;
+        System.out.println("Sub = :"+result);
+
+        result = num1 * num2;
+        System.out.println("Multiplicatio = :"+result);
+
+        result = num1 / num2;
+        System.out.println("Div = :"+result);
+
+        result = num1 % num2;
+        System.out.println("Remainder = :"+result);
+
+
+    }
+}
