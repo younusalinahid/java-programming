602763464b29e9f8e4dd173370bc4e7bc56471b2
nahid <eng.yanahid@gmail.com>
2023-02-06 21:45:39 +0600

    Odd number use While Loop

---
+++ src/WhileEvenNumber.java
@@ -0,0 +1,11 @@
+public class WhileEvenNumber {
+    public static void main(String[] args) {
+
+        int i=1;
+
+        while (i<=100){
+            System.out.println(i);
+            i= i+2;
+        }
+    }
+}
