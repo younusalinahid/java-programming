f74f03471cf25495f6191a848485ad0ca27adea8
nahid <eng.yanahid@gmail.com>
2023-02-06 14:23:51 +0600

    UnaryOp Plus & minus

---
+++ src/UnaryOperrator.java
@@ -0,0 +1,13 @@
+public class UnaryOperrator {
+    public static void main(String[] args) {
+
+        int result, x = 10;
+
+        result = +x;
+        System.out.println("result = "+result);
+
+        result = -x;
+        System.out.println("result = "+result);
+
+    }
+}
