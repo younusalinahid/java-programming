578fa5b51254493b97bbbc0dc37f4a6967b8d3c0
nahid <eng.yanahid@gmail.com>
2023-02-06 21:25:56 +0600

    Even Number

---
+++ src/EvenOdd.java
@@ -0,0 +1,13 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+
+        for(int i=2; i<=100; i=i+2)
+        {
+            System.out.println(i);
+        }
+
+
+
+
+    }
+}
