1c1eacdd736b80238b1f4b3cb47c9c77e1f50796
nahid <eng.yanahid@gmail.com>
2023-02-06 17:14:28 +0600

    Unary Increment and Decrement

---
+++ src/UnaryIncreDecre.java
@@ -0,0 +1,29 @@
+public class UnaryIncreDecre {
+
+    public static void main(String[] args) {
+
+        int x = 20, y;
+
+        y = x++; //post increment
+        System.out.println("y = "+y);
+
+        y = x;
+        System.out.println("y = "+y);
+
+        y = ++x; //pre increment
+        System.out.println("y = "+y);
+
+        y = x;
+        System.out.println("y = "+y);
+
+        y = x--;//pre Decrement
+        System.out.println("y = "+y);
+
+        y = x;
+        System.out.println("y = "+y);
+
+        y = --x;
+        System.out.println("y = "+y);
+
+    }
+}
