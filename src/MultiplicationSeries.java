dbb1f38a733db48579393e748f999da2d96898c8
nahid <eng.yanahid@gmail.com>
2023-02-07 21:10:11 +0600

    Series of Multiplication

--- src/MultiplicationSeries.java
+++ src/MultiplicationSeries.java
@@ -4,15 +4,15 @@ public class MultiplicationSeries {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int sum = 1, n;
+        int result  = 1, n;
 
         System.out.print("Enter the last number ");
         n = input.nextInt();
 
         for(int i=1; i<=n; i++){
             System.out.print(i+"x");
-            sum = sum * i;
+            result = result * i;
         }
-        System.out.println("\nThe sum is "+sum);
+        System.out.println("\nThe sum is "+result);
     }
 }
