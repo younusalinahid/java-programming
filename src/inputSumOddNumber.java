e84a078ecd6f0657d2397e42141fe43a6bbd31ca
nahid <eng.yanahid@gmail.com>
2023-02-07 19:33:52 +0600

    Sum of odd number

--- src/inputSumOddNumber.java
+++ src/inputSumOddNumber.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
 
-public class SumEvenNumber {
-
+public class inputSumOddNumber {
     public static void main(String[] args) {
 
-    //print even number
-        Scanner input = new Scanner(System.in);
-        int sum = 0, n;
-        System.out.print("Enter final number");
-        n = input.nextInt();
+        //Print sum off odd number
 
+        Scanner input = new Scanner(System.in);
+        int sum = 0,n;
+        System.out.println("Enter final number ");
+        n = input.nextInt();
 
         for(int i=1; i<=n; i++){
 
-            if(i%2==0) {
+            if(i%2!=0){
                 sum = sum + i;
-                System.out.println("   " + i);
+                System.out.println(i);
             }
+
         }
         System.out.println("The sum is : "+sum);
     }
