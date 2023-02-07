09a10ed703a78c2f4dc2fd999d6228d9e8b3fb0a
nahid <eng.yanahid@gmail.com>
2023-02-07 22:38:00 +0600

    Prime number

---
+++ src/PrimeNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int num, count = 0;
+
+        System.out.println("Enter any number ");
+        num = input.nextInt();
+
+        for (int i = 2; i < num; i++) {
+            if (num%i==0) {
+                count++;
+                break;
+            }
+        }
+        if (count == 0)
+            System.out.println("Prime number");
+        else {
+            System.out.println("Not prime number");
+        }
+    }
+}
+
