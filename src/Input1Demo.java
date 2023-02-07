291817af2d55e8f3512d8547c9613580d3aa530e
nahid <eng.yanahid@gmail.com>
2023-02-07 18:56:04 +0600

    Sum 1 to n number

---
+++ src/Input1Demo.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Input1Demo {
+    public static void main(String[] args) {
+
+        //Event 1 to n number
+        Scanner input = new Scanner(System.in);
+        int sum = 0,n;
+        System.out.println("Ener Final number ");
+        n = input.nextInt();
+
+        for(int i=1; i<=n; i++){
+            sum = sum +i;
+
+        }
+            System.out.println("The sum is "+sum);
+
+
+
+    }
+}
