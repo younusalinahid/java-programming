cea5d546965392937babcad0bcf36e106f0e7ddc
nahid <eng.yanahid@gmail.com>
2023-02-07 23:47:41 +0600

    Sum of Digit

---
+++ src/SumOfDigit.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SumOfDigit {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int temp, num, r, sum = 0;
+        System.out.println("Enter any number ");
+        num = input.nextInt();
+
+        temp = num;
+
+        while(temp!=0){
+
+            r = temp % 10;
+            sum = sum + r;
+            temp = temp / 10;
+
+        }
+        System.out.println("Sum of digit "+sum);
+
+
+
+
+    }
+}
