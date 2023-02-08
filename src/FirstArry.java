da3a0c386ae9347fa36f700294f47ebf108471d6
nahid <eng.yanahid@gmail.com>
2023-02-08 18:37:01 +0600

    First Array

---
+++ src/FirstArry.java
@@ -0,0 +1,17 @@
+public class FirstArry {
+
+    public static void main(String[] args) {
+
+        int[] number = new int[5];
+
+        number[0] = 10;
+        number[1] = 10;
+        number[2] = 10;
+        number[3] = 10;
+        number[4] = 10;
+
+        int sum = number[0] + number[1] + number[2] + number[3] + number[4];
+
+        System.out.println("Sum "+sum);
+    }
+}
