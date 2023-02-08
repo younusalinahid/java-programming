22966295975d086e8fb9dcd70e384e4b522b2a54
nahid <eng.yanahid@gmail.com>
2023-02-08 17:24:35 +0600

    Pattern Demo 03

---
+++ src/PatternDemo03.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PatternDemo03 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any number ");
+        int n = input.nextInt();
+
+        for (int row = n; row >=1; row--){
+            for (int col = 1; col <= row; col++){
+
+                System.out.print(" "+col);
+            }
+            System.out.println( );
+        }
+    }
+}
+
