4aefca4cbc58e13f9cf36fc58acc52b932c9fdb4
nahid <eng.yanahid@gmail.com>
2023-02-08 19:52:07 +0600

    Average of Array

---
+++ src/AverageArray.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class AverageArray {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        double[] number = new double[5];
+        double sum = 0;
+
+        System.out.print("Enter 5 number ");
+
+        for (int i = 0; i < 5; i++) {
+            number[i] = input.nextInt();
+        }
+        //number[0] = 1
+        //number[1] = 2
+        //number[2] = 3
+        //number[3] = 4
+        //number[4] = 5
+
+        for (int i = 0; i < 5; i++) {
+            sum = sum + number[i];
+        }
+        System.out.println("The sum is :" + sum);
+        double avg = sum / 5;
+        System.out.print("The Average is :" + avg);
+    }
+}
