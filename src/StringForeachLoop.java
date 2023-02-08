e733fdbb7a7f36630ff89256194442a0317fab24
nahid <eng.yanahid@gmail.com>
2023-02-08 20:53:30 +0600

    String for each loop

---
+++ src/StringForeachLoop.java
@@ -0,0 +1,11 @@
+public class StringForeachLoop {
+    public static void main(String[] args) {
+
+        String[] names = {"Younus","ali","Nahid","khan"};
+
+        for (String x : names){
+            System.out.println(x);
+        }
+    }
+
+}
