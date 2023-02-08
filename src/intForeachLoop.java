c45f117bb92219020bcdd08bb094f4cfa2ac2b88
nahid <eng.yanahid@gmail.com>
2023-02-08 21:01:05 +0600

    For each loop

---
+++ src/intForeachLoop.java
@@ -0,0 +1,14 @@
+public class intForeachLoop {
+
+    public static void main(String[] args) {
+
+        int[] num = {10,20,30,40,50,60,70,80,90,100};
+        int sum = 0;
+
+        for (int x : num){
+            sum = sum + x;
+        }
+        System.out.println(sum);
+    }
+
+}
