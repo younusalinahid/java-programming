f1a1891c5500c3316a95e51d3d92482b0e9c9bff
nahid <eng.yanahid@gmail.com>
2023-02-08 20:48:38 +0600

    Array Length

---
+++ src/StringArray.java
@@ -0,0 +1,19 @@
+
+public class StringArray{
+
+    public static void main(String[] args) {
+
+        String[] names = {"Younus","ali","Nahid","khan"};
+
+         System.out.println(names.length);
+        //lenght for Array sixe
+
+        for (int i=0; i<4; i++){
+            System.out.println(names[i]);
+        }
+    }
+
+
+}
+
+
