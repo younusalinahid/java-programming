851690323048e504c97261938ec23e587aff9673
nahid <eng.yanahid@gmail.com>
2023-02-09 19:41:59 +0600

    Array Descending

--- src/ArrayDescending.java
+++ src/ArrayDescending.java
@@ -1,17 +1,15 @@
 import java.util.Arrays;
 
-public class ArraySort {
+public class ArrayDescending {
     public static void main(String[] args) {
 
-        int[] number = {10, 20, 13, 15, 25};
+        int[] number = {10, -13, 13, 20, 17};
 
         Arrays.sort(number);
 
-        System.out.println("Ascending :");
-        for (int i=0; i<5; i++){
+        System.out.println("Descending : ");
+        for (int i=4; i>=0; i--){
             System.out.print(" "+number[i]);
         }
-
-
     }
 }
