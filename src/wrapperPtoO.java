45944923f56a620f20955ca3eca02a9a9173f305
nahid <eng.yanahid@gmail.com>
2023-02-10 01:51:39 +0600

    Wapper Object to Primitive

---
+++ src/wrapperPtoO.java
@@ -0,0 +1,17 @@
+public class wrapperPtoO {
+
+
+    public static void main(String[] args) {
+
+
+        //object to primitive data type
+        Double d = new Double(10.25);
+        System.out.println(d);
+
+
+        double e = d.doubleValue();
+        System.out.println(e);
+
+
+    }
+}
