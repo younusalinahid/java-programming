b33b462987fb814d8d5d76ad235b0f54d0033faa
nahid <eng.yanahid@gmail.com>
2023-02-09 22:45:49 +0600

    All over String

---
+++ src/StringDemo04.java
@@ -0,0 +1,19 @@
+public class StringDemo04 {
+
+    public static void main(String[] args) {
+
+        String s1 = "This is my country";
+        System.out.println(s1);
+
+        String s2 = s1.replace('i','j');
+        System.out.println(s2);
+
+        String[] s3 = s1.split(" ");
+        for (String x : s3){
+            System.out.println(x);
+        }
+
+    }
+
+
+}
