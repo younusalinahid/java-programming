6ee23e7eb474b2e621fb80936fa4148feae33adf
nahid <eng.yanahid@gmail.com>
2023-02-10 22:10:21 +0600

    Random number print

---
+++ src/RendomDemo.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class RendomDemo {
+
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+
+        int randomNumber = rand.nextInt(10);
+        System.out.println("Random number = "+randomNumber);
+
+
+
+
+
+    }
+}
