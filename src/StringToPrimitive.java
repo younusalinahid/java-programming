cde54234b3a68f5fd6dfcb5117085588cef97ddb
nahid <eng.yanahid@gmail.com>
2023-02-10 19:30:18 +0600

    String to Primitive

---
+++ src/StringToPrimitive.java
@@ -0,0 +1,12 @@
+public class StringToPrimitive {
+
+    public static void main(String[] args) {
+
+        String s = "32";
+        int i = Integer.parseInt(s);
+        System.out.println("i = "+i);
+
+
+
+    }
+}
