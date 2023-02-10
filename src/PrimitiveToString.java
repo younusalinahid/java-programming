3bafa3f9b9db63fb9ac61dabef53527782624faa
nahid <eng.yanahid@gmail.com>
2023-02-10 19:27:01 +0600

    Primitive to String

---
+++ src/PrimitiveToString.java
@@ -0,0 +1,13 @@
+public class PrimitiveToString {
+    public static void main(String[] args){
+
+        boolean b = true;
+        String s = Boolean.toString(b);
+        System.out.println("s = "+s);
+
+
+
+
+
+    }
+}
