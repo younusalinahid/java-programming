b7f98f32c6b1e8474c532692446fd042e06619eb
nahid <eng.yanahid@gmail.com>
2023-02-10 19:45:51 +0600

    Octal to Decimal

---
+++ src/octaltodecimal.java
@@ -0,0 +1,11 @@
+public class octaltodecimal {
+
+    public static void main(String[] args) {
+
+        String octal = "657";
+        Integer decimal = Integer.parseInt(octal, 8);
+        System.out.println(decimal);
+
+
+    }
+}
