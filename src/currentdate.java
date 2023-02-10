36d4bcc9cc5b7f3299fed1d9146c70df15aaaacc
nahid <eng.yanahid@gmail.com>
2023-02-10 21:52:06 +0600

    Current Date

---
+++ src/currentdate.java
@@ -0,0 +1,17 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class currentdate {
+
+    public static void main(String[] args) {
+
+        Date date = new Date();
+      //  System.out.println(date);
+
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/YYYY");
+        String currentDate = dateFormat.format(date);
+        System.out.println("Current date :"+currentDate);
+
+    }
+}
