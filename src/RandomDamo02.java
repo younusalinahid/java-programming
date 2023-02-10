c6f7ffe2f368af24f4432eea7198e5c8a58097f8
nahid <eng.yanahid@gmail.com>
2023-02-10 22:13:33 +0600

    Random number

---
+++ src/RandomDamo02.java
@@ -0,0 +1,9 @@
+public class RandomDamo02 {
+
+    public static void main(String[] args) {
+
+        int randomNumber = (int) (Math.random()*10);
+        System.out.println("Random number = "+randomNumber);
+
+    }
+}
