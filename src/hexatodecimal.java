473f6cc6f29c9ce0a445d236b3dae59ab60b87ed
nahid <eng.yanahid@gmail.com>
2023-02-10 19:48:27 +0600

    Hexadecimal to Decimal

---
+++ src/hexatodecimal.java
@@ -0,0 +1,11 @@
+public class hexatodecimal {
+
+    public static void main(String[] args) {
+
+        String hexadecimal = "A";
+        Integer decimal = Integer.parseInt(hexadecimal, 16);
+        System.out.println(decimal);
+
+
+    }
+}
