87ab1ca6a64b2a9cc830ffdf9cc5e433cff6d38e
nahid <eng.yanahid@gmail.com>
2023-02-10 19:39:47 +0600

    Binari to Decimal covert

---
+++ src/binarytodecimal.java
@@ -0,0 +1,11 @@
+public class binarytodecimal {
+
+    public static void main(String[] args) {
+
+        String binary = "1010";
+        Integer decimal = Integer.parseInt(binary, 2);
+        System.out.println(decimal);
+
+
+    }
+}
