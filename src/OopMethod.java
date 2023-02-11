4fbe9b4fd55c3b470e5bf1f4f27bb9f0b939e73f
nahid <eng.yanahid@gmail.com>
2023-02-11 14:40:04 +0600

    Oop Methord

---
+++ src/OopMethod.java
@@ -0,0 +1,14 @@
+public class OopMethod {
+
+    String name,gender;
+    int phone;
+
+    void displayInformation() {
+
+        System.out.println("Name : " + name);
+        System.out.println("Gender : " + gender);
+        System.out.println("Phone : " + phone);
+    }
+
+
+}
---
+++ src/TestM.java
@@ -0,0 +1,22 @@
+public class TestM {
+    public static void main(String[] args) {
+
+        Teacher teacher1 = new Teacher();
+
+        teacher1.name = "Younus ali";
+        teacher1.gender = "male";
+        teacher1.phone = 1234567890;
+        teacher1.displayInformation();
+
+        System.out.println();
+        Teacher teacher2 = new Teacher();
+
+        teacher2.name = "Md.nahid";
+        teacher2.gender = "male";
+        teacher2.phone = 1710442700;
+        teacher2.displayInformation();
+
+
+
+    }
+}
