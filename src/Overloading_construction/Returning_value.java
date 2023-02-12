d5050847b30ef40792cb8c7ed4c2262b4bb5af1c
nahid <eng.yanahid@gmail.com>
2023-02-12 20:39:35 +0600

    Returning value

---
+++ src/Overloading_construction/Returning_value.java
@@ -0,0 +1,11 @@
+package Overloading_construction;
+
+public class Returning_value {
+
+    int square (int value){
+        return value*value;
+
+    }
+
+
+}
---
+++ src/Overloading_construction/test2.java
@@ -0,0 +1,13 @@
+package Overloading_construction;
+
+public class test2 {
+    public static void main(String[] args) {
+        Returning_value ob1 = new Returning_value();
+        System.out.println("Squre of 5 = "+ob1.square(5));
+
+        Returning_value ob2 = new Returning_value();
+        System.out.println("Squre of 6 = "+ob1.square(6));
+
+
+    }
+}
