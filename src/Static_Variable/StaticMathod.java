2e32e5c9d89f2f463016b094854d75afeaf670f2
nahid <eng.yanahid@gmail.com>
2023-02-15 01:17:52 +0600

    Static method 03

---
+++ src/Static_Variable/StaticMathod.java
@@ -0,0 +1,13 @@
+package Static_Variable;
+
+public class StaticMathod {
+
+    void display(){
+        System.out.println("Iam non static method ");
+    }
+    static void display2(){
+        System.out.println("I am static method");
+    }
+
+
+}
---
+++ src/Static_Variable/Test04.java
@@ -0,0 +1,12 @@
+package Static_Variable;
+
+public class Test04 {
+    public static void main(String[] args) {
+
+        StaticMathod ob1 = new StaticMathod();
+        ob1.display();
+
+        StaticMathod.display2();
+
+    }
+}
