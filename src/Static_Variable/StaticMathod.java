002ef322f5ddfd3cc212e8117ef031ee1936d57e
nahid <eng.yanahid@gmail.com>
2023-02-15 16:10:26 +0600

    Static variable 04

--- src/Static_Variable/StaticMathod.java
+++ src/Static_Variable/StaticMathod.java
@@ -2,10 +2,15 @@ package Static_Variable;
 
 public class StaticMathod {
 
-    void display(){
+    static int x = 10;
+    void display()
+    {
+
         System.out.println("Iam non static method ");
     }
     static void display2(){
+
+        System.out.println(" "+x);
         System.out.println("I am static method");
     }
 
--- src/Static_Variable/Test04.java
+++ src/Static_Variable/Test04.java
@@ -3,8 +3,8 @@ package Static_Variable;
 public class Test04 {
     public static void main(String[] args) {
 
-        StaticMathod ob1 = new StaticMathod();
-        ob1.display();
+       // StaticMathod ob1 = new StaticMathod();
+        //ob1.display();
 
         StaticMathod.display2();
 
