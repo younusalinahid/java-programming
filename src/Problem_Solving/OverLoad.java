c273c65a00f5812779e0f3e0ee8f6df080f7b577
nahid <eng.yanahid@gmail.com>
2023-02-15 21:22:33 +0600

    Automatic Type Conversion

--- src/Problem_Solving/OverLoad.java
+++ src/Problem_Solving/OverLoad.java
@@ -2,9 +2,7 @@ package Problem_Solving;
 
 public class OverLoad {
 
-    void add (int a,int b){
-        System.out.println(a+b);
-    }
+
     void add (double a,double b){
         System.out.println(a+b);
     }
