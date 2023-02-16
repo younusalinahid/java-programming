03988364d26393296d9c6fa0ebd6ae36c1e9d673
nahid <eng.yanahid@gmail.com>
2023-02-16 16:17:54 +0600

    Recursion using factorial

---
+++ src/Oop/RecursionDemo.java
@@ -0,0 +1,17 @@
+package Oop;
+
+public class RecursionDemo {
+
+    int fact(int n){
+
+        if(n==1)
+            return 1;
+        else
+            return n*fact(n-1);
+    }
+
+
+
+}
+
+
---
+++ src/Oop/RecursionDemoTest.java
@@ -0,0 +1,15 @@
+package Oop;
+
+public class RecursionDemoTest {
+
+    public static void main(String[] args) {
+
+        RecursionDemo ob = new RecursionDemo();
+        int result = ob.fact(5);
+        System.out.println("Factorial 5 = "+result);
+
+        int result2 = ob.fact(6);
+        System.out.println("Factorial 5 = "+result2);
+    }
+
+}
