de0efbef5e265a79551aa80867073266d421711b
nahid <eng.yanahid@gmail.com>
2023-02-16 20:23:09 +0600

    Setter And Getter methods

---
+++ src/Oop/PersonEncapsulationTest.java
@@ -0,0 +1,16 @@
+package Oop;
+
+public class PersonEncapsulationTest {
+
+    public static void main(String[] args) {
+
+        Person p1 = new Person();
+        p1.setName("Nahid");
+        p1.setAge(21);
+        System.out.println(p1.getName());
+        System.out.println(p1.getAge());
+
+
+
+    }
+}
