638868b4f0093ee912d947089faf1bf07b888439
nahid <eng.yanahid@gmail.com>
2023-02-17 20:25:06 +0600

    this keyword 02

--- src/Oop/ThisKeyword.java
+++ src/Oop/ThisKeyword.java
@@ -3,16 +3,24 @@ package Oop;
 public class ThisKeyword {
 
     String name;
+    String hairColor;
     int age;
+
 
     ThisKeyword(String name,int age){
         this.name = name;
         this.age = age;
     }
+    ThisKeyword(String name,int age,String hairColor){
+        this(name, age);
+        this.hairColor = hairColor;
+
+    }
 
     void display(){
         System.out.println("Name : "+name);
         System.out.println("Age : "+age);
+        System.out.println("HairColor : "+hairColor);
     }
 
 }
--- src/Oop/ThisKeywordTest.java
+++ src/Oop/ThisKeywordTest.java
@@ -1,11 +1,16 @@
 package Oop;
 
+import java.util.Timer;
+
 public class ThisKeywordTest {
 
     public static void main(String[] args) {
 
         ThisKeyword p1 = new ThisKeyword("Younus ali",20);
         p1.display();
+
+        ThisKeyword p2 = new ThisKeyword("Rubal",22,"black");
+        p2.display();
     }
 
 }
