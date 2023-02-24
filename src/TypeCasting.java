d45edd84f8098c8694f54ec206c14750314c6ef2
nahid <eng.yanahid@gmail.com>
2023-02-25 00:58:00 +0600

    Type Custing

---
+++ src/TypeCasting.java
@@ -0,0 +1,19 @@
+public class TypeCasting {
+
+    public static void main(String[] args) {
+
+        //widening type casting(smoll to large)
+        int x = 10;
+        double y = x;
+        System.out.println(y);
+
+
+        //Narrowing type casting(Large to smoll)
+        double m = 10.5;
+        int n = (int) m;
+        System.out.println(n);
+
+
+    }
+
+}
