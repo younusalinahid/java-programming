2ebbb1f37573cdd80dda3820fde8724178e3a206
nahid <eng.yanahid@gmail.com>
2023-02-25 22:00:44 +0600

    Linked list 01

---
+++ src/Oop/LinkeListDemo.java
@@ -0,0 +1,19 @@
+package Oop;
+
+import java.util.LinkedList;
+
+public class LinkeListDemo {
+
+    public static void main(String[] args) {
+        LinkedList<String> countryNames = new LinkedList<String>();
+
+        countryNames.add("Bangladesh");
+        countryNames.add("Pakisten");
+        countryNames.add("India");
+        countryNames.add("Afghannustan");
+
+        System.out.println(countryNames);
+
+    }
+
+}
