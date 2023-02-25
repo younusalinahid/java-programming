7cb19693744c119e9f6cf0388886e31152e038eb
nahid <eng.yanahid@gmail.com>
2023-02-25 12:35:20 +0600

    Exception

---
+++ src/ExceptionDemo.java
@@ -0,0 +1,21 @@
+public class ExceptionDemo {
+
+    public static void main(String[] args) {
+        try{
+            int x = 10;
+            int y = 0;
+            int result = x/y;
+            System.out.println("result : "+result);
+
+        }catch (ArrayIndexOutOfBoundsException e1){
+            System.out.println("Exception : "+e1);
+        }catch (ArithmeticException e2){
+            System.out.println("Exception : "+e2);
+        }
+        finally {
+            System.out.println("Last line of the program  ");
+        }
+
+    }
+
+}
