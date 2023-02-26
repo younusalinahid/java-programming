5d1cacaacd9f6d86d66d9f2239de01b35d98f5c8
nahid <eng.yanahid@gmail.com>
2023-02-26 19:44:42 +0600

    Hashset

---
+++ src/HashsetDemo.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+
+public class HashsetDemo {
+
+    public static void main(String[] args) {
+
+        HashSet<String> fruitsName = new HashSet<String>();
+
+        fruitsName.add("Apple");
+        fruitsName.add("Orange");
+        fruitsName.add("Banana");
+        fruitsName.add("Mango");
+
+        System.out.println(fruitsName);
+        System.out.println("Size "+fruitsName.size());
+
+        fruitsName.remove("Apple");
+        System.out.println(fruitsName);
+
+        fruitsName.clear();
+        System.out.println(fruitsName);
+
+        boolean value = fruitsName.isEmpty();
+        System.out.println(value);
+
+    }
+}
