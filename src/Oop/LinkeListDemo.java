84668c787ac9e08f7032a1cd247181b4fb7a69a6
nahid <eng.yanahid@gmail.com>
2023-02-26 17:56:41 +0600

    Linked list 02

--- src/Oop/LinkeListDemo.java
+++ src/Oop/LinkeListDemo.java
@@ -11,7 +11,26 @@ public class LinkeListDemo {
         countryNames.add("Pakisten");
         countryNames.add("India");
         countryNames.add("Afghannustan");
+        countryNames.add("Napal");
+        countryNames.add(5,"Brazil");
+        countryNames.addFirst("Australia");
+        countryNames.addLast("Jepan");
+        //countryNames.remove("Japan");
+        //countryNames.remove(7);
+        //countryNames.removeFirst();
+        //countryNames.removeLast();
 
+
+        for (String country : countryNames){
+            System.out.println(country);
+        }
+
+        System.out.println("Size of the Linkedlist : "+countryNames.size());
+
+        System.out.println("First element : "+countryNames.getFirst());
+        System.out.println("Last element : "+countryNames.getLast());
+
+        countryNames.clear();
         System.out.println(countryNames);
 
     }
