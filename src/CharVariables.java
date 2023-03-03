94575c8ccf60dcb4e4184f62c5dba487327d93ab
nahid <eng.yanahid@gmail.com>
2023-03-03 16:33:12 +0600

    Char Variables

---
+++ src/CharVariables.java
@@ -0,0 +1,12 @@
+public class CharVariables {
+
+    public static void main(String[] args) {
+
+        char first = 65, second = 66, third = 67;
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+    }
+
+}
