034f72a9854651cbec80740c221d9ecef0599e81
nahid <eng.yanahid@gmail.com>
2023-03-03 16:08:45 +0600

    String 01

---
+++ src/Study.java
@@ -0,0 +1,19 @@
+public class Study {
+
+    public static void main(String[] args) {
+
+        String FirstName = "Younus ";
+        String SecondName = "Ali ";
+        String LastName = "Nahid";
+
+        String FullName = FirstName + SecondName + LastName;
+
+        System.out.println(FullName);
+
+
+
+
+
+    }
+
+}
