6f7d1e0aa27c8eba1aabff502f36ad6b1d041e94
nahid <eng.yanahid@gmail.com>
2023-03-29 22:56:22 +0600

    Static Method

---
+++ src/Oop/StaticMethod.java
@@ -0,0 +1,13 @@
+package Oop;
+
+public class StaticMethod {
+
+    void display1 (){
+        System.out.println("I am non static method ");
+    }
+
+    static void display2(){
+        System.out.println("I am static method");
+    }
+
+}
---
+++ src/Oop/StaticMethodTest.java
@@ -0,0 +1,13 @@
+package Oop;
+
+public class StaticMethodTest {
+
+    public static void main(String[] args) {
+
+        StaticMethod ob1 = new StaticMethod();
+        ob1.display1();
+
+        StaticMethod.display2();
+    }
+
+}
