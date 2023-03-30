71467bdba7a89706e2b9bf07b2beac0beb75ca0c
nahid <eng.yanahid@gmail.com>
2023-03-30 15:50:41 +0600

    MethodOver loading

---
+++ src/Oop/MethodOverloadingTest.java
@@ -0,0 +1,18 @@
+package Oop;
+
+public class MethodOverloadingTest {
+
+    public static void main(String[] args) {
+
+        MethodOverloading ob = new MethodOverloading();
+        ob.add();
+
+        ob.add(5,5);
+
+        ob.add(5,6,2);
+
+        ob.add(5,10,20);
+
+    }
+
+}
