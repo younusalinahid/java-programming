70a9f00223cf4df7395ca62de884fc8e69354ac4
nahid <eng.yanahid@gmail.com>
2023-03-30 16:45:36 +0600

    Encapsulation (Setter and Getter)

---
+++ src/Oop/EncapsulationTest.java
@@ -0,0 +1,16 @@
+package Oop;
+
+public class EncapsulationTest {
+
+    public static void main(String[] args) {
+
+        Encapsulation ob = new Encapsulation();
+        ob.setName("MD.Younus");
+        ob.setAge(21);
+
+        System.out.println(ob.getName());
+        System.out.println(ob.getAge());
+
+    }
+
+}
