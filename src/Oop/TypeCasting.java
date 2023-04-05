37ac11ceaee466bddb8b78032372f68c2318e73a
nahid <eng.yanahid@gmail.com>
2023-04-05 23:24:36 +0600

    Type Casting (Upcasting & Downcasting)

---
+++ src/Oop/TypeCasting.java
@@ -0,0 +1,16 @@
+package Oop;
+
+public class TypeCasting {
+
+    public static void main(String[] args) {
+
+
+        double y = 10.5;
+        int x = (int) y;
+        System.out.println(x);
+    }
+
+    //double to int
+    // thats call type casting
+
+}
