4cfcad1b3d187c88a51525829ad99bbcdf17147f
nahid <eng.yanahid@gmail.com>
2023-04-10 20:13:45 +0600

    String comparison | equal( ) vs ==

--- src/StringComparision.java
+++ src/StringComparision.java
@@ -7,6 +7,12 @@ public class StringComparision {
      String password3 = new String("Nahid123");
      String password4 = new String("Nahid45");
 
+
+        //(==)operator
+        System.out.println(password1==password2);
+        System.out.println(password1==password3);
+        System.out.println(password3==password1);
+        //equal method
         System.out.println(password1.equalsIgnoreCase(password2));
         System.out.println(password1.equals(password3));
         System.out.println(password3.equals(password4));
