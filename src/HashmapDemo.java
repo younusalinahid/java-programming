d6961adc93670e671788a6055b931cb7ecdddb47
nahid <eng.yanahid@gmail.com>
2023-04-11 15:52:38 +0600

    HasMap Demo

--- src/HashmapDemo.java
+++ src/HashmapDemo.java
@@ -4,13 +4,15 @@ public class HashmapDemo {
 
     public static void main(String[] args) {
         //put,get
-        HashMap <Integer,String> customer = new HashMap<Integer,String>();
+       HashMap<Integer,String> customer = new HashMap<Integer,String>();
 
-        customer.put(82,"Nahid");
-        customer.put(83,"Nadim");
-        customer.put(84,"Nahida");
+       customer.put(82,"Younus");
+       customer.put(83,"Ali");
+       customer.put(84,"Nahid");
 
+        System.out.println(customer.get(82));
         System.out.println(customer.get(83));
+        System.out.println(customer.get(84));
 
     }
 
