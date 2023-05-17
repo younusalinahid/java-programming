9ce95f551b3d69a8f46fd260ad9fa47687fccc62
nahid <eng.yanahid@gmail.com>
2023-05-17 21:47:35 +0600

    Array Demo

---
+++ src/ArrayDemo.java
@@ -0,0 +1,18 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        int[] number = new int[5];
+        number[0] = 10;
+        number[1] = 10;
+        number[2] = 10;
+        number[3] = 10;
+        number[4] = 10;
+
+        int sum = number[0] + number[1] + number[2] + number[3] + number[4];
+        System.out.println(sum);
+
+        int len = number.length;
+        System.out.println(len);
+
+    }
+}
